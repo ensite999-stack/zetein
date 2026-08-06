400e62d3dcaef289b574a76972cede072a04e77f
ensite999-stack <ensite999@gmail.com>
2026-08-06 21:26:24 +0800

    Update navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,36 +1,27 @@
 import { Logo } from "./logo";
 import { NavigationSheet } from "./navigation-sheet";
 
-
 export function Navbar() {
   return (
-    <header
-      className="
-      w-full
-      border-b
-      bg-background
-      "
-    >
-
+    <header className="w-full border-b bg-background">
       <div
         className="
-        flex
-        h-16
-        w-full
-        items-center
-        justify-between
-        px-4
-        sm:px-6
-        lg:px-8
+          flex
+          h-16
+          w-full
+          items-center
+          justify-between
+          px-4
+          sm:px-6
+          lg:px-8
         "
       >
-
+        {/* Logo */}
         <Logo />
 
+        {/* Mobile Menu */}
         <NavigationSheet />
-
       </div>
-
     </header>
   );
 }
