f3269fd6f2f794b8a84b897a36dc7340b575ffed
ensite999-stack <ensite999@gmail.com>
2026-08-06 21:00:29 +0800

    Update navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,18 +3,14 @@ import { NavMenu } from "./nav-menu";
 
 export function Navbar() {
   return (
-    <header className="w-full border-b bg-background">
-      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6">
+    <header className="w-full">
+      <div className="flex h-16 w-full items-center justify-between px-4">
 
-        {/* Left Logo */}
-        <div className="flex items-center">
-          <Logo />
-        </div>
+        {/* 左侧 Logo */}
+        <Logo />
 
-        {/* Right Menu */}
-        <div className="hidden md:flex">
-          <NavMenu />
-        </div>
+        {/* 右侧菜单 */}
+        <NavMenu />
 
       </div>
     </header>
