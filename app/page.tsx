8b3d65e72d7a54a2e2358539e03e9ed99154b964
ensite999-stack <ensite999@gmail.com>
2026-08-06 20:28:17 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
-import FAQ from "@/components/faq";
-import Features from "@/components/features";
-import Footer from "@/components/footer";
 import Hero from "@/components/hero";
-import { Navbar } from "@/components/navbar";
-import Pricing from "@/components/pricing";
-import Testimonial from "@/components/testimonial";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
-      <Features />
-      <FAQ />
-      <Testimonial />
-      <Pricing />
       <Footer />
     </>
   );
