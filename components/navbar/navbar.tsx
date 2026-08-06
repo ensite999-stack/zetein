d69a3d7943f6685e0b2aeb4737fe422ad0dc5a3a
ensite999-stack <ensite999@gmail.com>
2026-08-06 20:44:35 +0800

    Update navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,38 +1,20 @@
-import Image from "next/image";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
 
 export function Navbar() {
   return (
-    <header className="w-full border-b">
+    <header className="w-full border-b bg-background">
       <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6">
 
-        {/* Logo 左侧 */}
+        {/* Left Logo */}
         <div className="flex items-center">
-          <Image
-            src="/zetein-logo.png"
-            alt="Zetein"
-            width={120}
-            height={40}
-            className="h-10 w-auto object-contain"
-          />
+          <Logo />
         </div>
 
-
-        {/* Menu 右侧 */}
-        <nav className="flex items-center gap-6 text-sm">
-
-          <a href="/login">
-            Login
-          </a>
-
-          <a href="/register">
-            Register
-          </a>
-
-          <a href="/about">
-            About Zetein
-          </a>
-
-        </nav>
+        {/* Right Menu */}
+        <div className="hidden md:flex">
+          <NavMenu />
+        </div>
 
       </div>
     </header>
