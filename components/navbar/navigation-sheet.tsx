3c9ad655723f79a9d0996a51b3d8781c24cfeeaa
ensite999-stack <ensite999@gmail.com>
2026-08-06 21:15:32 +0800

    Refactor NavigationSheet component structure

--- components/navbar/navigation-sheet.tsx
+++ components/navbar/navigation-sheet.tsx
@@ -1,37 +1,27 @@
-import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { VisuallyHidden as VisuallyHiddenPrimitive } from "radix-ui";
 import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Logo } from "./logo";
 import { NavMenu } from "./nav-menu";
 
-export const NavigationSheet = () => {
+export function NavigationSheet() {
   return (
     <Sheet>
-      <VisuallyHiddenPrimitive.Root>
-        <SheetTitle>Navigation Drawer</SheetTitle>
-      </VisuallyHiddenPrimitive.Root>
       <SheetTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="ghost" size="icon">
           <Menu />
         </Button>
       </SheetTrigger>
+
       <SheetContent>
         <Logo />
-        <NavMenu orientation="vertical" className="mt-12" />
 
-        <div className="mt-8 space-y-4">
-          <Button variant="outline" className="w-full sm:hidden">
-            Sign In
-          </Button>
-          <Button className="w-full xs:hidden">Get Started</Button>
-        </div>
+        <NavMenu
+          orientation="vertical"
+          className="mt-12"
+        />
+
       </SheetContent>
     </Sheet>
   );
-};
+}
