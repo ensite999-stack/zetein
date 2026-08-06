2379dc1097d06f21df79437b61495d6de2fee302
ensite999-stack <ensite999@gmail.com>
2026-08-06 23:04:20 +0800

    Adjust logo dimensions and styles in Navbar

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 
+
 export function Logo() {
   return (
-    <div className="flex items-center gap-2">
+    <div
+      className="
+        flex
+        items-center
+        gap-2
+      "
+    >
 
       <Image
         src="/zetein-logo.jpg"
         alt="Zetein"
-        width={42}
-        height={42}
+        width={40}
+        height={40}
         className="
-          h-10
-          w-10
+          h-9
+          w-9
           object-contain
         "
-        priority
       />
+
 
       <span
         className="
