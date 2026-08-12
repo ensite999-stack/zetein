245e901df4616a38d14282b41093b125b1cf85fa
ensite999-stack <ensite999@gmail.com>
2026-08-12 20:53:11 +0800

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,77 @@
+export default function Loading() {
+
+
+  return (
+
+    <main
+
+      className="
+      flex
+      min-h-screen
+      items-center
+      justify-center
+      bg-[#050505]
+      text-white
+      "
+
+    >
+
+
+      <div
+
+        className="
+        text-center
+        "
+
+      >
+
+
+        <h1
+
+          className="
+          text-6xl
+          font-black
+          tracking-[-0.06em]
+          "
+
+        >
+
+          Zetein
+
+          <span
+            className="cursor-blink"
+          >
+
+            _
+
+          </span>
+
+
+        </h1>
+
+
+
+        <p
+
+          className="
+          mt-6
+          text-sm
+          text-white/40
+          "
+
+        >
+
+          Exploring the world of .xyz
+
+        </p>
+
+
+      </div>
+
+
+    </main>
+
+  );
+
+
+}
