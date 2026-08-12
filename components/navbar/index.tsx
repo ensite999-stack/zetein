df7154ad3cfb5d17b201ba0d77032427b62a5322
ensite999-stack <ensite999@gmail.com>
2026-08-12 20:08:00 +0800

    Update index.tsx

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
-
 export default function Navbar() {
 
   return (
@@ -13,44 +10,11 @@ export default function Navbar() {
       top-0
       left-0
       z-50
+      h-20
       w-full
       bg-transparent
       "
-    >
-
-      <div
-        className="
-        mx-auto
-        flex
-        h-20
-        max-w-6xl
-        items-center
-        justify-between
-        px-6
-        "
-      >
-
-        <Link
-          href="/"
-          className="
-          text-xl
-          font-black
-          tracking-[-0.05em]
-          text-white
-          "
-        >
-
-          Zetein
-          <span className="cursor-blink">
-            _
-          </span>
-
-        </Link>
-
-
-      </div>
-
-    </header>
+    />
 
   );
 
