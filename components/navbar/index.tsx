c6d509311d6689990459d1d5d1ff6c7a94491824
ensite999-stack <ensite999@gmail.com>
2026-08-12 20:47:49 +0800

    Refactor Navbar component layout and styles

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,21 +1,63 @@
 "use client";
 
+import Link from "next/link";
+
+
 export default function Navbar() {
+
 
   return (
 
     <header
+
       className="
       fixed
-      top-0
       left-0
+      top-0
       z-50
-      h-20
       w-full
       bg-transparent
       "
-    />
+
+    >
+
+
+      <nav
+
+        className="
+        mx-auto
+        flex
+        h-20
+        max-w-7xl
+        items-center
+        justify-center
+        px-6
+        "
+
+      >
+
+
+        <Link
+
+          href="/"
+
+          className="
+          hidden
+          "
+
+        >
+
+          Zetein
+
+        </Link>
+
+
+      </nav>
+
+
+    </header>
 
   );
+
 
 }
